15e9071d4a6d4ed2c883b0f57ce2c7ac0843b426
achen229 <achen228@163.com>
2020-04-19 13:03:01 +0800

    命名管道-读

---
+++ ipc/fifo_read.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main()
+{
+    // int mkfifo(const char *pathname, mode_t  mode);
+    // pathname:管道文件名称
+    // mode:管道文件权限
+    // 返回值：0  失败：-1
+    char *fifo = "./test.fifo";
+    umask(0);
+    int ret = mkfifo(fifo, 0664); 
+    if (ret < 0)
+    {
+        if (errno != EEXIST)
+        {
+            perror("mkfifo error");
+            return -1;
+        }
+    }
+
+    printf("start open --------\n");
+    int fd = open(fifo, O_RDONLY);
+    printf("end open --------\n");
+    if (fd < 0)
+    {
+        perror("open error");
+        return -1;
+    }
+    printf("fifo:%s open success!\n", fifo);
+
+    while (1)
+    {
+        char buf[1024] = {0};
+        read(fd, buf, 1023);
+        printf("peer say:%s\n", buf);
+    }
+    close(fd);
+    return 0;
+}
