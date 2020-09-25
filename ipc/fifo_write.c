8a9bdbbd7f992a7afc0927f160c3f431d7cfc6a1
achen228 <achen228@163.com>
2020-09-25 19:51:40 +0800

    fifo_write.c--修改

--- ipc/fifo_write.c
+++ ipc/fifo_write.c
@@ -12,35 +12,30 @@ int main()
     // pathname:管道文件名称
     // mode:管道文件权限
     // 返回值：0  失败：-1
-    char *fifo = "./test.fifo";
+    const char* fifo = "./test.fifo";
     umask(0);
     int ret = mkfifo(fifo, 0664); 
-    if (ret < 0)
+    if (ret < 0 && errno != EEXIST)
     {
-        if (errno != EEXIST)
-        {
-            perror("mkfifo error");
-            return -1;
-        }
-    }
-
-    printf("start open --------\n");
-    int fd = open(fifo, O_WRONLY);
-    printf("end open --------\n");
-    if (fd < 0)
-    {
-        perror("open error");
+        perror("mkfifo error");
         return -1;
     }
-    printf("fifo:%s open success!\n", fifo);
+
+    int fd = open(fifo, O_WRONLY);
+    if (fd < 0)
+    {
+        perror("open fifo error");
+        return -1;
+    }
+    printf("open fifo success\n");
    
+    int i = 0;
     while (1)
     {
-        char buf[1024] = {0};
-        printf("I say:");
-        fflush(stdout);
-        scanf("%s,", buf);
-        write(fd, buf, strlen(buf));
+       char buf[1024] = {0};
+       sprintf(buf, "hello world---%d", i++);
+       write(fd, buf, strlen(buf));
+       sleep(1);
     }
     close(fd);
     return 0;
