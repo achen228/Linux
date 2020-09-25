6e3e8de9dc086bd6c79424680dba645b05fd6a4a
achen228 <achen228@163.com>
2020-09-25 19:51:04 +0800

    fifo_read.c--修改

--- ipc/fifo_read.c
+++ ipc/fifo_read.c
@@ -12,33 +12,39 @@ int main()
     // pathname:管道文件名称
     // mode:管道文件权限
     // 返回值：0  失败：-1
-    char *fifo = "./test.fifo";
+    const char* fifo = "./test.fifo";
     umask(0);
     int ret = mkfifo(fifo, 0664); 
-    if (ret < 0)
+    if (ret < 0 && errno != EEXIST)
     {
-        if (errno != EEXIST)
-        {
-            perror("mkfifo error");
-            return -1;
-        }
-    }
-
-    printf("start open --------\n");
-    int fd = open(fifo, O_RDONLY);
-    printf("end open --------\n");
-    if (fd < 0)
-    {
-        perror("open error");
+        perror("mkfifo error");
         return -1;
     }
-    printf("fifo:%s open success!\n", fifo);
+
+    int fd = open(fifo, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open fifo error");
+        return -1;
+    }
+    printf("open fifo success\n");
 
     while (1)
     {
         char buf[1024] = {0};
-        read(fd, buf, 1023);
-        printf("peer say:%s\n", buf);
+        int ret = read(fd, buf, sizeof(buf) - 1);
+        if(ret < 0)
+        {
+            perror("read error");
+            return -1;
+        }
+        else if(ret == 0)
+        {
+            printf("all write is closed\n");
+            return -1;
+        }
+
+        printf("read buf is:%s\n", buf);
     }
     close(fd);
     return 0;
