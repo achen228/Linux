ebc6db52d57ff3ae7f52341e7c16ee59a6c4feb3
achen228 <achen228@163.com>
2020-09-25 18:50:35 +0800

    pipe2.c--修改

--- ipc/pipe2.c
+++ ipc/pipe2.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 
 int main()
 {
-    // ls | grep Make
+    // ls -l | grep pipe.c
     int pipefd[2];
     int ret = pipe(pipefd);
     if (ret < 0)
@@ -16,27 +15,33 @@ int main()
         perror("pipe error");
         return -1;
     }
+
     int pid1 = fork();
     if (pid1 == 0)
     {
         // child
-        // ls
-        close(pipefd[0]);
+        //将标准输出重定向到管道写入端，向1写入数据就相当于向管道写入数据
         dup2(pipefd[1], 1);
-        execlp("ls", "ls", NULL);
+        execlp("ls", "ls", "-l", NULL);
         exit(0);
     }
+
     int pid2 = fork();
     if (pid2 == 0)
     {
-        // grep Make
+        // grep pipe.c
+        // 关闭写端，ls -l进程退出后，所有写端就会被关闭
         close(pipefd[1]);
+        //将标准输入重定向到管道读端，从0读取数据就相当于从管道读取数据
         dup2(pipefd[0], 0);
-        execlp("grep", "grep", "Make", NULL);
+        execlp("grep", "grep", "pipe.c", NULL);
         exit(0);
     }
+
+    //关闭父进程的读写端
     close(pipefd[1]);
     close(pipefd[0]);
+    // 等待子进程退出
     waitpid(pid1, NULL, 0);
     waitpid(pid2, NULL, 0);
     
